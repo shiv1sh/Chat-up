18ce3981e959c33765894b553673a56f05feb6ac
Shivansh Kashyap <shivkasht2001@gmail.com>
2023-07-13 18:05:22 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,50 @@
+const socket=io();
+const name=prompt("Enter your name");
+let textarea=document.querySelector("#textarea");
+let messageArea=document.querySelector('.message_area');
+textarea.addEventListener('keyup',(e)=>{
+    if(e.key==='Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg={
+        user:name,
+        message:message.trim()
+    }
+
+    // Append 
+    appendMessage(msg,'outgoing')
+    textarea.value=''
+    scrollToBottom();
+
+    // Send to Server
+    socket.emit('message',msg)
+}
+
+function appendMessage(msg,type){
+    let mainDiv=document.createElement('div');
+
+    let className=type;
+    mainDiv.classList.add(className,'message')
+
+    let markup=`
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML=markup;
+    messageArea.appendChild(mainDiv);
+}
+
+
+// Recieve message
+
+socket.on('message',(msg)=>{
+    appendMessage(msg,'incoming');
+    scrollToBottom();
+})
+function scrollToBottom(){
+    messageArea.scrollTop=messageArea.scrollHeight
+}
